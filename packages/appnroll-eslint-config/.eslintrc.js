93e164d5e988409654b8e901f5267bb92ff75b0b
Kamil Poprawa <kamilxunai@yahoo.com>
2020-10-28 12:12:59 +0100

    add curly rules

--- packages/appnroll-eslint-config/.eslintrc.js
+++ packages/appnroll-eslint-config/.eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
     "react/prop-types": "off",
     "react/jsx-boolean-value": "error",
     "@typescript-eslint/no-var-requires": "off",
+    "curly": "error",
   },
   settings: {
     react: {
